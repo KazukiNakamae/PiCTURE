9d2799ed1d976d2e126cdacacc18539001520768
KazukiNakamae <kazukinakamae@gmail.com>
2025-12-15 02:42:34 +0900

    Updated get_intersection_variants.sh

--- PiCTURE/Docker/get_intersection_variants.sh
+++ PiCTURE/Docker/get_intersection_variants.sh
@@ -100,15 +100,15 @@ fi
 if [[ ! -f 8_vcf_identification/${set_name}.hg38.identified.vcf ]]; then
     echo "Intersect VCF..."
 
-    # Build JEXL: all samples must be called and non-reference (not HomRef)
+    # Build JEXL: all samples must be non-reference (not HomRef)
     jexl=""
     for s in "${input_name_arr[@]}"; do
-    cond="(vc.getGenotype(\"${s}\").isCalled() && ! vc.getGenotype(\"${s}\").isHomRef())"
-    if [[ -z "${jexl}" ]]; then
-        jexl="${cond}"
-    else
-        jexl="${jexl} && ${cond}"
-    fi
+        cond="!vc.getGenotype('${s}').isHomRef()"
+        if [[ -z "${jexl}" ]]; then
+            jexl="${cond}"
+        else
+            jexl="${jexl} && ${cond}"
+        fi
     done
     echo "[INFO] JEXL = ${jexl}"
 
@@ -116,10 +116,11 @@ if [[ ! -f 8_vcf_identification/${set_name}.hg38.identified.vcf ]]; then
     -u "$(id -u $USER):$(id -g $USER)" \
     -v /etc/passwd:/etc/passwd:ro \
     -v /etc/group:/etc/group:ro \
-    --name nakamae_haplotypecaller --memory 120g -itv $PWD:/data -w /data --rm broadinstitute/gatk:4.3.0.0 \
+    --memory 120g -itv "$PWD:/data" -w /data --rm broadinstitute/gatk:4.3.0.0 \
     gatk SelectVariants \
         -V "8_vcf_identification/${set_name}.merge.vcf" \
         -O "8_vcf_identification/${set_name}.hg38.identified.vcf" \
+        --max-nocall-number 0 \
         -select "${jexl}" \
         -R "5_recal_data/resources-broad-hg38-v0-Homo_sapiens_assembly38.fasta"
 fi
