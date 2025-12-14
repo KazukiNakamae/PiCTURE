95642d141ec12fb15c15689af42eff039d736f6f
KazukiNakamae <kazukinakamae@gmail.com>
2025-12-15 03:06:16 +0900

    Updated get_intersection_variants.sh

--- PiCTURE/Docker/get_intersection_variants.sh
+++ PiCTURE/Docker/get_intersection_variants.sh
@@ -1,133 +1,126 @@
 #!/bin/bash
+set -euo pipefail
+
 if [ $# -lt 4 ]; then
-     echo "3 arguments are required.";
-     exit 1;
+  echo "Usage: $0 <working_directory> <set_name> <sample_label_1> <sample_label_2> [sample_label_3 ...]"
+  exit 1
 fi
 
 output=$1
 set_name=$2
-# サンプル名を配列で取得
+
+# sample labels (used for filenames and (after reheader) sample names in VCF)
 args=("$@")
-start_index=2
-end_index=${#args[@]}
-input_name_arr=("${args[@]:$start_index:$end_index}")
+input_name_arr=("${args[@]:2}")
 
-if [[ ! -e $output ]]; then
-      echo ${output}" cannot be found.";
-      exit 1;
+if [[ ! -e "${output}" ]]; then
+  echo "[ERROR] ${output} cannot be found."
+  exit 1
 fi
-cd $output;
 
-# get intersection varitnts set
+cd "${output}"
 
-file_name_arr=();
+# -------------------------------
+# 1) Ensure per-sample VCFs are indexed/compressed
+#    Expected inputs:
+#      8_vcf_identification/<label>.hg38.identified.vcf
+# -------------------------------
 for element in "${input_name_arr[@]}"; do
-  # file_name="--variant 8_vcf_identification/${element}.hg38.identified.vcf"
-  file_name="8_vcf_identification/${element}.hg38.identified.vcf.gz"
-  file_name_arr+=("$file_name")
+  if [[ ! -f "8_vcf_identification/${element}.hg38.identified.vcf" ]]; then
+    echo "[ERROR] Missing input VCF: 8_vcf_identification/${element}.hg38.identified.vcf"
+    echo "        (This script assumes per-sample identified VCFs already exist.)"
+    exit 1
+  fi
+
+  if [[ ! -f "8_vcf_identification/${element}.hg38.identified.vcf.idx" ]]; then
+    echo "Indexing VCF (GATK IndexFeatureFile): ${element}"
+    docker run --rm -it       -u "$(id -u "${USER}")":"$(id -g "${USER}")"       -v /etc/passwd:/etc/passwd:ro       -v /etc/group:/etc/group:ro       -v "${PWD}":/data -w /data       broadinstitute/gatk:4.3.0.0       gatk IndexFeatureFile         -I "8_vcf_identification/${element}.hg38.identified.vcf"
+  fi
+
+  if [[ ! -f "8_vcf_identification/${element}.hg38.identified.vcf.gz" ]]; then
+    echo "Compressing VCF (bgzip via bcftools): ${element}"
+    docker run --rm -it       -u "$(id -u "${USER}")":"$(id -g "${USER}")"       -v /etc/passwd:/etc/passwd:ro       -v /etc/group:/etc/group:ro       -v "${PWD}":/data -w /data       staphb/bcftools:1.16       bcftools view -Oz         "8_vcf_identification/${element}.hg38.identified.vcf"         -o "8_vcf_identification/${element}.hg38.identified.vcf.gz"
+
+    echo "Indexing VCF (tabix): ${element}"
+    docker run --rm -it       -u "$(id -u "${USER}")":"$(id -g "${USER}")"       -v /etc/passwd:/etc/passwd:ro       -v /etc/group:/etc/group:ro       -v "${PWD}":/data -w /data       biocontainers/tabix:v1.9-11-deb_cv1       tabix -p vcf "8_vcf_identification/${element}.hg38.identified.vcf.gz"
+  fi
 done
 
-for element in "${input_name_arr[@]}"; do
-    if [[ ! -f 8_vcf_identification/${element}.hg38.identified.vcf.idx ]]; then
-        echo "Indexing VCF..."
-        docker run \
-            -u "$(id -u $USER):$(id -g $USER)" \
-            -v /etc/passwd:/etc/passwd:ro \
-            -v /etc/group:/etc/group:ro \
-            --name nakamae_haplotypecaller --memory 120g -itv $PWD:/data -w /data --rm broadinstitute/gatk:4.3.0.0 \
-            gatk IndexFeatureFile \
-            -I 8_vcf_identification/${element}.hg38.identified.vcf;
-    fi
-done
-
-# if [[ ! -f 8_vcf_identification/${set_name}.merge.vcf ]]; then
-#     echo "Merge VCF..."
-#     docker run \
-#         -u "$(id -u $USER):$(id -g $USER)" \
-#         -v /etc/passwd:/etc/passwd:ro \
-#         -v /etc/group:/etc/group:ro \
-#         --name nakamae_haplotypecaller --memory 120g -itv $PWD:/data -w /data --rm broadinstitute/gatk:4.3.0.0 \
-#         gatk CombineGVCFs \
-#         ${file_name_arr[@]} \
-#         -O 8_vcf_identification/${set_name}.merge.vcf \
-#         -R 5_recal_data/resources-broad-hg38-v0-Homo_sapiens_assembly38.fasta;
-# fi
-# if [[ ! -f 8_vcf_identification/${set_name}.merge.vcf ]]; then
-#     echo "Error."
-#     exit 1;
-# fi
-
-
-
-
-
+# -------------------------------
+# 2) Reheader (rename) samples so that:
+#    - bcftools merge does NOT hit "Duplicate sample names"
+#    - JEXL that refers to sample labels is valid
+#    This is critical when the VCF header sample name does not match your label.
+# -------------------------------
+merge_inputs=()
 
 for element in "${input_name_arr[@]}"; do
-    if [[ ! -f 8_vcf_identification/${element}.hg38.identified.vcf.gz ]]; then
-        echo "Compressing VCF..."
-        docker run \
-            -u "$(id -u $USER):$(id -g $USER)" \
-            -v /etc/passwd:/etc/passwd:ro \
-            -v /etc/group:/etc/group:ro \
-            --name nakamae_vaf_calculation --memory 120g -itv $PWD:/data -w /data --rm staphb/bcftools:1.16 \
-            bcftools view -Oz 8_vcf_identification/${element}.hg38.identified.vcf \
-            -o 8_vcf_identification/${element}.hg38.identified.vcf.gz;
-        echo "Indexing VCF..."
-        docker run \
-            -u "$(id -u $USER):$(id -g $USER)" \
-            -v /etc/passwd:/etc/passwd:ro \
-            -v /etc/group:/etc/group:ro \
-            --name nakamae_vaf_calculation --memory 120g -itv $PWD:/data -w /data --rm biocontainers/tabix:v1.9-11-deb_cv1 \
-            tabix -p vcf 8_vcf_identification/${element}.hg38.identified.vcf.gz;
-    fi
+  in_vcf="8_vcf_identification/${element}.hg38.identified.vcf.gz"
+
+  # Query sample names in the VCF
+  vcf_samples=$(docker run --rm -i     -u "$(id -u "${USER}")":"$(id -g "${USER}")"     -v /etc/passwd:/etc/passwd:ro     -v /etc/group:/etc/group:ro     -v "${PWD}":/data -w /data     staphb/bcftools:1.16     bcftools query -l "${in_vcf}" | tr -d '\r')
+
+  n_samples=$(printf "%s\n" "${vcf_samples}" | sed '/^$/d' | wc -l | awk '{print $1}')
+
+  if [[ "${n_samples}" -ne 1 ]]; then
+    echo "[WARN] ${in_vcf} contains ${n_samples} samples (expected 1). Skipping reheader."
+    merge_inputs+=("${in_vcf}")
+    continue
+  fi
+
+  old_name=$(printf "%s" "${vcf_samples}" | head -n 1)
+
+  if [[ "${old_name}" == "${element}" ]]; then
+    merge_inputs+=("${in_vcf}")
+    continue
+  fi
+
+  echo "[INFO] Reheader: ${old_name} -> ${element} (${in_vcf})"
+  name_file="8_vcf_identification/${element}.new_sample_name.txt"
+  printf "%s\n" "${element}" > "${name_file}"
+
+  out_vcf="8_vcf_identification/${element}.hg38.identified.renamed.vcf.gz"
+  if [[ ! -f "${out_vcf}" ]]; then
+    docker run --rm -it       -u "$(id -u "${USER}")":"$(id -g "${USER}")"       -v /etc/passwd:/etc/passwd:ro       -v /etc/group:/etc/group:ro       -v "${PWD}":/data -w /data       staphb/bcftools:1.16       bcftools reheader -s "${name_file}" "${in_vcf}" -Oz -o "${out_vcf}"
+
+    docker run --rm -it       -u "$(id -u "${USER}")":"$(id -g "${USER}")"       -v /etc/passwd:/etc/passwd:ro       -v /etc/group:/etc/group:ro       -v "${PWD}":/data -w /data       biocontainers/tabix:v1.9-11-deb_cv1       tabix -f -p vcf "${out_vcf}"
+  fi
+
+  merge_inputs+=("${out_vcf}")
 done
-if [[ ! -f 8_vcf_identification/${set_name}.merge.vcf ]]; then
-    echo "Merge VCF..."
-    docker run \
-        -u "$(id -u $USER):$(id -g $USER)" \
-        -v /etc/passwd:/etc/passwd:ro \
-        -v /etc/group:/etc/group:ro \
-        --name nakamae_vaf_calculation --memory 120g -itv $PWD:/data -w /data --rm staphb/bcftools:1.16 \
-        bcftools merge --merge all \
-        ${file_name_arr[@]} \
-        -O v -o 8_vcf_identification/${set_name}.merge.vcf;
-fi
-if [[ ! -f 8_vcf_identification/${set_name}.merge.vcf ]]; then
-    echo "Error."
-    exit 1;
+
+# -------------------------------
+# 3) Merge into multi-sample VCF
+# -------------------------------
+if [[ ! -f "8_vcf_identification/${set_name}.merge.vcf" ]]; then
+  echo "Merge VCF..."
+  docker run --rm -it     -u "$(id -u "${USER}")":"$(id -g "${USER}")"     -v /etc/passwd:/etc/passwd:ro     -v /etc/group:/etc/group:ro     -v "${PWD}":/data -w /data     staphb/bcftools:1.16     bcftools merge --merge all       "${merge_inputs[@]}"       -O v -o "8_vcf_identification/${set_name}.merge.vcf"
 fi
 
-if [[ ! -f 8_vcf_identification/${set_name}.hg38.identified.vcf ]]; then
-    echo "Intersect VCF..."
-
-    # Build JEXL: all samples must be non-reference (not HomRef)
-    jexl=""
-    for s in "${input_name_arr[@]}"; do
-    cond="(vc.getGenotype(\"${s}\").isCalled()&&!vc.getGenotype(\"${s}\").isHomRef())"
-    if [[ -z "${jexl}" ]]; then
-        jexl="${cond}"
-    else
-        jexl="${jexl}&&${cond}"
-    fi
-    done
-    echo "[INFO] JEXL = ${jexl}"
-
-    docker run \
-    -u "$(id -u $USER):$(id -g $USER)" \
-    -v /etc/passwd:/etc/passwd:ro \
-    -v /etc/group:/etc/group:ro \
-    --memory 120g -itv "$PWD:/data" -w /data --rm broadinstitute/gatk:4.3.0.0 \
-    gatk --java-options "-DGATK_STACKTRACE_ON_USER_EXCEPTION=true" SelectVariants \
-        -V "8_vcf_identification/${set_name}.merge.vcf" \
-        -O "8_vcf_identification/${set_name}.hg38.identified.vcf" \
-        -select "${jexl}" \
-        -R "5_recal_data/resources-broad-hg38-v0-Homo_sapiens_assembly38.fasta"
-
+if [[ ! -f "8_vcf_identification/${set_name}.merge.vcf" ]]; then
+  echo "[ERROR] Merge failed."
+  exit 1
 fi
-if [[ ! -f 8_vcf_identification/${set_name}.hg38.identified.vcf ]]; then
-    echo "Error."
-    exit 1;
+
+# -------------------------------
+# 4) Intersection using Method C (JEXL on merged multi-sample VCF)
+#    Recommended robust expression:
+#      - vc.hasGenotypes(): sanity check
+#      - getNoCallCount()==0 : all samples called
+#      - getHomRefCount()==0 : no sample is HomRef => all samples are non-reference (Het/HomVar)
+# -------------------------------
+if [[ ! -f "8_vcf_identification/${set_name}.hg38.identified.vcf" ]]; then
+  echo "Intersect VCF..."
+
+  jexl="vc.hasGenotypes() && vc.getNoCallCount() == 0 && vc.getHomRefCount() == 0"
+  echo "[INFO] JEXL = ${jexl}"
+
+  docker run --rm -it     -u "$(id -u "${USER}")":"$(id -g "${USER}")"     -v /etc/passwd:/etc/passwd:ro     -v /etc/group:/etc/group:ro     -v "${PWD}":/data -w /data     broadinstitute/gatk:4.3.0.0     gatk SelectVariants       -V "8_vcf_identification/${set_name}.merge.vcf"       -O "8_vcf_identification/${set_name}.hg38.identified.vcf"       -select "${jexl}"       -R "5_recal_data/resources-broad-hg38-v0-Homo_sapiens_assembly38.fasta"
+fi
+
+if [[ ! -f "8_vcf_identification/${set_name}.hg38.identified.vcf" ]]; then
+  echo "[ERROR] Intersect step failed."
+  exit 1
 fi
 
 echo "Done."
-exit 0;
